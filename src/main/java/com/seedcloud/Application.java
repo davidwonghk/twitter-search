0d8fa9761656884d5a1a982619d057816fda4808
josh1313 <josh1313@hotmail.com.hk>
2016-04-19 12:09:18 +0000

    add schedule task

--- src/main/java/com/seedcloud/Application.java
+++ src/main/java/com/seedcloud/Application.java
@@ -3,13 +3,11 @@ package com.seedcloud;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 public class Application {
    public static void main(String[] args) {
       ApplicationContext context = 
              new ClassPathXmlApplicationContext("applicationContext.xml");
-
-      TwitterSearchTask task = (TwitterSearchTask) context.getBean("twitterSearchTask");
-      task.run();
 
    }
 }
--- src/main/java/com/seedcloud/dao/SearchResultDao.java
+++ src/main/java/com/seedcloud/dao/SearchResultDao.java
@@ -3,6 +3,9 @@ package com.seedcloud.dao;
 import java.util.Date;
 import twitter4j.Status;
 
+/**
+ * Data Access Object to store the search result from Twitter API
+ */
 public interface SearchResultDao {
     void insert(Date searchTime, Status tweet);
 }
